c5fa426f0eb6f8fedd3330c4aba02c14ccd788b0
islamManzoor <47377484+islamManzoor@users.noreply.github.com>
2020-03-31 15:07:16 +0500

    First commit from eclipse.

---
+++ gitEclipse/src/gitEclipsePackage/gitEclipseClass.java
@@ -0,0 +1,12 @@
+package gitEclipsePackage;
+
+public class gitEclipseClass {
+
+	public static void main(String[] args) {
+		
+		//Short cut for bellow. Write sysout and than press the ctrl + space
+		System.out.println("Say hello to GIT using Eclipse");
+
+	}
+
+}
